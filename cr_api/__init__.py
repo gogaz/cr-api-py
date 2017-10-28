e23beed0b55ac063e4cde67ba1a006832a178a05
SML <smlbiobot@gmail.com>
2017-10-28 16:49:15 +0800

    updated version, removed setup

--- cr_api/__init__.py
+++ cr_api/__init__.py
@@ -1,7 +1,7 @@
 """
 Clash Royale wrapper for cr-api.com
 """
-__version__ = "0.5"
+__version__ = "0.6"
 
 from .client import Client
 from .exceptions import APITimeoutError, APIClientResponseError, APIError
